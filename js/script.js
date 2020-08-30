96ea3dbb97bfd30943154209763eb9d6b033c2a8
abirasik <abirasik@gmail.com>
2020-08-30 16:32:17 +0600

    add_scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//smooth scroll
+
+var html_body = $('html, body');
+    $('#navbarSupportedContent> ul > li a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 80
+                }, 1200);
+                return false;
+            }
+        }
+    });
+
+    $(".navbar-collapse a").click(function(){
+  $(".navbar-collapse").collapse('hide')
+})
+
 new WOW().init();
 
 $(window).scroll(function () {
